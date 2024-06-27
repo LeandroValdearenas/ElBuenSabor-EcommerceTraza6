ae5c538341a0b21416be05b0eb5dfd426fe02aea
Leandro <leandrovaldearenas@gmail.com>
2024-06-26 21:53:55 -0300

    Arreglada vigencia de promociones de tipo Happy Hour

--- src/componentes/productos/verificarVigenciaPromociones.tsx
+++ src/componentes/productos/verificarVigenciaPromociones.tsx
@@ -6,25 +6,27 @@ export const verificarVigenciaPromociones = (promocion: Promocion) => {
     const [horaCierre, minutoCierre, segundoCierre] = promocion.horaHasta.toString().split(':').map(Number);
     
     if (promocion.tipoPromocion === 'Promocion') {
-
+        // PROMOCIÓN
         const comienzoPromocion = new Date(promocion.fechaDesde);
         comienzoPromocion.setHours(horaApertura, minutoApertura, segundoApertura, 0);
         const finPromocion = new Date(promocion.fechaHasta);
         finPromocion.setHours(horaCierre, minutoCierre, segundoCierre, 0);
 
-        // Comparar la fecha actual con el horario de apertura y cierre
+        // Comparar la fecha actual con las fechas de inicio y cierre de la promoción
         return (fecha >= comienzoPromocion && (fecha <= finPromocion || promocion.horaHasta.toString() === '00:00:00'));
 
     } else {
-
+        // HAPPY HOUR
         const inicioHappyHour = new Date(fecha);
         inicioHappyHour.setHours(horaApertura, minutoApertura, segundoApertura, 0);
         const cierreHappyHour = new Date(fecha);
         cierreHappyHour.setHours(horaCierre, minutoCierre, segundoCierre, 0);
+        const fechaComienzo = new Date(promocion.fechaDesde);
+        const fechaFin = new Date(promocion.fechaHasta);
 
-        // Comparar la fecha actual con el horario de apertura y cierre
+        // Comparar la fecha actual y el horario para el HappyHour
         return (
-            (fecha >= promocion.fechaDesde && fecha <= promocion.fechaHasta) 
+            (fecha >= fechaComienzo && fecha <= fechaFin) 
             && (fecha >= inicioHappyHour && (fecha <= cierreHappyHour || promocion.horaDesde.toString() === '00:00:00'))
         );
     }
